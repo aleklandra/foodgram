7ca60e0ea3925e6b01a8a99f4265c575ea37298e
Aleksandra Petrova <aleklandra@yandex.ru>
2024-09-11 12:32:56 +0300

    short link

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -104,8 +104,8 @@ class RecipeViewSet(ModelViewSet):
         get_object_or_404(Recipe, id=pk)
         try:
 
-            short_link = shorten_url(
-                request.build_absolute_uri('/api/recipes/' + pk))
+            _, short_link = shorten_url(
+                request.build_absolute_uri('/recipes/' + pk))
         except NoReverseMatch:
             return Response({'detail': 'Не найдено'},
                             status=status.HTTP_404_NOT_FOUND)
